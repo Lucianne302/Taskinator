bd49dc37876fc0c6cde48e6062c3a52556736e41
Lucianne302 <Lucianne21@comcast.net>
2020-08-25 18:01:18 -0400

    added event object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHadler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault(); 
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
-buttonEl.addEventListener("click", createTaskHadler);
+formEl.addEventListener("submit", createTaskHandler);
 
