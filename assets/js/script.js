2c63b5f839996fa7bcf155047d20a25c783fd50e
Lucianne302 <Lucianne21@comcast.net>
2020-08-26 19:35:50 -0400

    updating tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var taskIdCounter = 0; 
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+var pageContentEl = document.querySelector("#page-content");
 
 var TaskFormHandler = function(event) {
 
@@ -98,4 +99,24 @@ var createTaskActions = function(taskId) {
 };
 
 formEl.addEventListener("submit", TaskFormHandler);
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if (event.target.matches(".delete-btn")) {
+        // get the element's task id
+        var taskId = event.target.getAttribute("data-task-id");
+        console.log("taskId");
+        //deleteTask("taskId");
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    //console.log(taskId);
+    taskSelected.remove();
+};
+
+pageContentEl.addEventListener("click", taskButtonHandler);
+
 
