decee11a451a927dca27396786ecf1c6ae825929
Lucianne302 <Lucianne21@comcast.net>
2020-08-27 15:35:24 -0400

    added drag and drop reset style

--- assets/js/script.js
+++ assets/js/script.js
@@ -207,6 +207,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
 
@@ -229,6 +230,14 @@ var dropTaskHandler = function(event) {
     }
 
     dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
+};
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
 };
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -236,3 +245,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
